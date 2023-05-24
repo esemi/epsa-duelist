f23437408700d065e17f96ffa56145aec5bb45e5
Simon <spam@esemi.ru>
2023-05-25 01:08:06 +0200

    :sparkles: Use pydantic for settings

--- app/bot.py
+++ app/bot.py
@@ -1,24 +1,29 @@
 import asyncio
 import logging
-from config import TOKENmain
-from aiogram import Bot, Dispatcher, types
-from aiogram.filters.command import Command
 
-logging.basicConfig(level=logging.INFO)
-bot = Bot(TOKENmain)
+from aiogram import Bot, Dispatcher, types, filters
 
-dp = Dispatcher()
+from app.settings import app_settings
 
-@dp.message(Command("start"))
+bot = Bot(app_settings.telegram_token)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(filters.Command("start"))
 async def cmd_start(message: types.Message):
     await message.answer("Привет!")
 
-@dp.message(Command("help"))
+
+@dp.message_handler(filters.Command("help"))
 async def cmd_start(message: types.Message):
     await message.answer("Тут будет помощь")
+
 
 async def main():
     await dp.start_polling(bot)
 
+
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+
     asyncio.run(main())
---
+++ app/settings.py
@@ -0,0 +1,15 @@
+"""Application settings."""
+import os
+
+from pydantic import BaseSettings
+
+
+class AppSettings(BaseSettings):
+    """Application settings class."""
+
+    telegram_token: str
+
+
+app_settings = AppSettings(
+    _env_file=os.path.join(os.path.dirname(__file__), '..', '.env'),
+)
