c4a90a789ea2f509fc4520039aeb6dd10907de18
Ivan <122587908+wai-AI@users.noreply.github.com>
2023-06-17 16:56:54 +0300

    Delete test_cmd_help.py

--- tests/bot/tests/bot/test_cmd_help.py
+++
@@ -1,9 +0,0 @@
-from unittest.mock import AsyncMock
-
-from app.bot import cmd_help
-
-
-async def test_cmd_help_smoke(message_mock: AsyncMock):
-    await cmd_help(message_mock)
-
-    assert message_mock.answer.call_count == 1
